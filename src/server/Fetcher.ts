dba995dd7e8c64b2536736882c5f04cbd3a4a88d
MJ <newwonmj@gmail.com>
2023-12-06 17:26:37 +0900

    json 수정

--- src/server/Fetcher.ts
+++ src/server/Fetcher.ts
@@ -11,30 +11,32 @@ import { ApiConfig } from "../api/ApiConfig";
 export const getMoviesData = async () => {
   const response = await ApiConfig.get(
     // `movie/popular?language=en-US&page=1`
-    `trending/movie/week?language=ko&page=1`
+    // `trending/movie/week?language=ko&page=1`
+    `popular`
   ).then((result: any) => result.data.results);
   return response;
 };
 
 export const getTopRatedMoviesData = async () => {
   const response = await ApiConfig.get(
-    `movie/top_rated?language=en-US&page=1`
+    // `movie/top_rated?language=en-US&page=1`
+    `rated`
   ).then((result: any) => result.data.results);
-
   return response;
 };
 
 export const getUpcomingMoviesData = async () => {
   const response = await ApiConfig.get(
-    `movie/upcoming?language=en-US&page=1`
+    // `movie/upcoming?language=en-US&page=1`
+    `upcoming`
   ).then((result: any) => result.data.results);
-
   return response;
 };
 
 export const getGenreMoviesData = async () => {
   const response = await ApiConfig.get(
-    `genre/movie/list?language=ko&page=1`
+    // `genre/movie/list?language=ko&page=1`
+    `genre`
   ).then((result: any) => result.data.genres);
   return response;
 };
